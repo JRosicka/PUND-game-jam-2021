2b35a8e20ef0c15ee27120b1d0b422de57078759
Intermittent <tylerjkerch@gmail.com>
2021-07-24 13:50:57 -0500

    Added script component to dialog contraption so I can actually do stuff with it

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -11,7 +11,7 @@ public class DialogController : MonoBehaviour
     private TextMeshProUGUI dialogText;
     private TMP_Text activeText;
 
-    public GameObject dialog;
+    private GameObject dialog;
 
     // Start is called before the first frame update
     void Start()
