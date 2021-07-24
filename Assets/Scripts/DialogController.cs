5a6645afd238b80e0ce3521c8f13e637fce90113
Intermittent <tylerjkerch@gmail.com>
2021-07-24 17:00:56 -0500

    Implemented new scene with prototype HUD, whoever's in charge of the main scene can delete the old one

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -16,7 +16,12 @@ public class DialogController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Grab relevant children
+        dialog = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+
+        // Clear text box
+        dialogText = dialog.GetComponent<TextMeshProUGUI>();
+        dialogText.text = "";
     }
 
     // Update is called once per frame
@@ -25,8 +30,28 @@ public class DialogController : MonoBehaviour
         
     }
 
-    public void DisplayDialog(DialogLine newLine)
+    public void DisplayDialog(DialogLine activeLine)
     {
+        dialogText.text = activeLine.dialogString;
+        activeText = dialog.GetComponent<TMP_Text>();
+        StartCoroutine(RevealByCharacter(activeText));
+    }
 
+    IEnumerator RevealByCharacter(TMP_Text textComponent)
+    {
+        textComponent.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = textComponent.textInfo;
+
+        int totalVisibleCharacters = textInfo.characterCount; // Get # of Visible Character in text object
+        int visibleCount = 0;
+
+        while (visibleCount <= totalVisibleCharacters)
+        {
+            textComponent.maxVisibleCharacters = visibleCount; // How many characters should TextMeshPro display?
+            visibleCount += 1;
+
+            yield return new WaitForSecondsRealtime(textDelay);
+        }
     }
 }
