91dcfe2a0d5958da7d1e5a9fec71efd9c1444368
Jacob Rabchenia <jr.archangelstudios@gmail.com>
2021-07-26 02:59:10 -0500

    Made a Win Screen. It's reeeeal good. Make sure your gizmos are off so it doesn't spoil the suprise.

---
+++ Assets/Scripts/Camera Dolly/OutroMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutroMover : MonoBehaviour
+{
+    private const string SHOOT_NAME = "Shoot";
+
+    [Range(0f, 10f)] public float cameraMoveSpeed = 2.5f;
+    public Rail rail;
+    
+    private int currentSeg;
+    private float transition;
+
+    private void Update()
+    {
+        if(!rail)
+        {
+            return;
+        }
+
+        Play();
+;
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / cameraMoveSpeed;
+        if(transition >1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if(transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.LinearPosition(currentSeg, transition);
+        transform.rotation = rail.Orientation(currentSeg, transition);
+
+    }
+}
---
+++ Assets/Scripts/DataRetriever.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataRetriever : MonoBehaviour
+{
+    public Transform node;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        node.position = new Vector3(PlayerPrefs.GetInt("cameraPosX"), PlayerPrefs.GetInt("cameraPosY"), PlayerPrefs.GetInt("cameraPosZ"));
+        node.rotation = new Quaternion(PlayerPrefs.GetInt("cameraRotX"), PlayerPrefs.GetInt("cameraRotY"),PlayerPrefs.GetInt("RotZ"),PlayerPrefs.GetInt("RotW"));
+    }
+}
